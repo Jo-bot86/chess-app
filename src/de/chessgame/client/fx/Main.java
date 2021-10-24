dfbf5837186d9e72dec87c81f4b8b7e7d8418531
Josef Weldemariam <j.weldemariam@web.de>
2021-10-24 13:52:57 +0200

    comment changed

--- src/de/chessgame/client/fx/Main.java
+++ src/de/chessgame/client/fx/Main.java
@@ -7,13 +7,9 @@ import javafx.application.Application;
  * Die in dieser Klasse enthaltene main-Mehtode dient als Einstiegspunkt in die Anwendung.
  * <br><br>
  * <u><b>HINWEIS</b></u><br>
- * Das Programm enthält noch nicht alle Reglen. So ist es noch möglich invalide Züge zu machen.
- * Z.b. ist ein König in der Lage sich selbst ins Schach zu stellen. Das muss somit vom Benutzer selbst
- * vermieden werden. Darüber hinaus ist eine Figur die gepinnt ist, ebenfalls in der Lage ihren Posten zu
- * verlassen und somit den König sich selbst zu überlassen. Außerdem wird nicht erkannt, dass
- * eine Figur den König schützen kann, indem sie die Schachgebende Figur blockt. Diese Figur hat dann keinen
- * validen Zug und ist nicht frei gegeben um sie zu bewegen. Desweiteren ist es noch nicht möglich eine Bauern
- * en passant zu schlagen sowie eine Rochade auszuführen.
+ * Das Programm verfügt über einen vollständigen user-support. D.h. es ist nicht möglich nicht-legale
+ * Züge auszuführen. Um ein vollwertiges Schachspiel zu sein, fehlt einzig die Funktion einen Bauern umwandeln
+ * zu können, wenn dieser das Brett vollständig überquert hat.
  * @author Josef Weldemariam
  *
  */
